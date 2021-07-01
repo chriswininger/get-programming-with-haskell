147616227aa258a2ea1ea18036a9ffce4d957b66
Chris Wininger <chris.wininger@gmail.com>
2021-07-01 13:40:57 -0400

    go

--- chp17/chp17.hs
+++ chp17/chp17.hs
@@ -40,6 +40,8 @@ instance Semigroup Color where
              | all (`elem` [Red,Yellow,Orange]) [a,b] = Orange
              | otherwise = Brown
 
+-- if either a or b is an element of Red,Blue,Purple then purple
+
 type Events = [String]
 type Probs = [Double]
 
@@ -56,3 +58,39 @@ showPair event prob = mconcat [event, "|", show prob, "\n"]
 instance Show PTable where
     show (PTable events probs) = mconcat pairs
         where pairs = zipWith showPair events probs
+
+cartCombine :: (a -> b -> c) -> [a] -> [b] -> [c]
+cartCombine func l1 l2 = zipWith func newL1 cycledL2
+    where nToAdd = length l2
+          repeatedL1 = map (take nToAdd . repeat) l1
+          newL1 = mconcat repeatedL1
+          cycledL2 = cycle l2
+
+combineEvents :: Events -> Events -> Events
+combineEvents e1 e2 = cartCombine combiner e1 e2
+    where combiner = (\x y -> mconcat [x,"-",y])
+
+combineProbs :: Probs -> Probs -> Probs
+combineProbs p1 p2 = cartCombine (*) p1 p2
+
+instance Semigroup PTable where
+    (<>) ptable1 (PTable [] []) = ptable1
+    (<>) (PTable [] []) ptable2 = ptable2
+    (<>) (PTable e1 p1) (PTable e2 p2) = createPTable newEvents newProbs
+        where newEvents = combineEvents e1 e2
+              newProbs = combineProbs p1 p2
+
+instance Monoid PTable where
+    mempty = PTable [] []
+    mappend = (<>)
+
+coin :: PTable
+coin = createPTable ["heads", "tails"] [0.5, 0.5]
+
+spinner :: PTable
+spinner = createPTable ["red","blue","green"] [0.1,0.2,0.7]
+
+-- run `coin <> spinner` in ghci
+-- coin <> coin
+-- coin <> coin <> coin
+-- mconcat [coin, coin, coin]
